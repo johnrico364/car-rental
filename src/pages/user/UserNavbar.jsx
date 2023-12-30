357a672b051ed2eb0169fb3353f7c9849eeffd48
John Anthony <johnrico364@gmail.com>
2023-12-30 14:08:10 +0800

    Remove border class

--- src/pages/user/UserNavbar.jsx
+++ src/pages/user/UserNavbar.jsx
@@ -3,7 +3,7 @@ import { Outlet, Link } from "react-router-dom";
 
 export const UserNavbar = () => {
   return (
-    <div className="container-fluid main-background border px-0">
+    <div className="container-fluid main-background px-0">
       <div className="d-flex px-2">
         <div className="w-25 mt-3 " id="logo-container">
           <img src="/images/Logo.png" height={50} alt="" />

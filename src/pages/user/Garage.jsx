12eff2cf8457550675e66ee78c3ee746db24791e
John Anthony <johnrico364@gmail.com>
2024-01-01 13:22:49 +0800

    Style: Garage UI

--- src/pages/user/Garage.jsx
+++ src/pages/user/Garage.jsx
@@ -1,3 +1,30 @@
-export const Garage = () =>{
-    return <div className="display-1">This is garage</div>
-}
+export const Garage = () => {
+  return (
+    <div className="route-height-container animate__animated animate__backInUp">
+      <div className="container-fluid">
+        <div className="garage-title ps-4 pt-4">Cars up for grabs!</div>
+        <div className="garage-items-container px-3 overflow-auto">
+          <div className="row px-3 justify-content-center">
+            <div className="card col-lg-3 m-2">
+              <img
+                src="/images/Car Images.jpg"
+                className="card-img-top"
+                alt="..."
+              />
+              <div className="card-body ">
+                <div className="card-text">
+                  <b>Name:</b> Toyota Supra
+                </div>
+                <div className="card-text">
+                  <b>Fare:</b> P 2000/day
+                </div>
+              </div>
+            </div>
+            
+            
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};

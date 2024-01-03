7188b2a5b8b30a3d2f3582b02615119568c31e02
John Anthony <johnrico364@gmail.com>
2024-01-03 20:12:42 +0800

    Add: Add Car UI

--- src/pages/admin/AddCar.jsx
+++ src/pages/admin/AddCar.jsx
@@ -1,8 +1,50 @@
 export const AddCar = () => {
-    return (
-      <div className="route-height-container">
-        <div className="container-fluid border">hello world add car</div>
+  return (
+    <div className="route-height-container">
+      <div className="container-fluid border">
+        <div className="add-car-title">
+          Time to rev things up - let's add a car
+        </div>
+        <div className="row justify-content-center ">
+          <div className="col-7">
+            <div className="row">
+              <div className="col-12 mt-4 mb-2 px-0">
+                <input
+                  type="text"
+                  className="input-Container w-100"
+                  placeholder="Add Car"
+                />
+              </div>
+              <div className="col-6 my-2 ps-0 pe-1">
+                <select className="input-Container w-100">
+                  <option>Sedan</option>
+                  <option>SUV</option>
+                  <option>Coupe</option>
+                </select>
+              </div>
+              <div className="col-6 my-2 ps-1 pe-0">
+                <input
+                  type="text"
+                  className="input-Container w-100"
+                  placeholder="Color"
+                />
+              </div>
+              <div className="col-12 my-2 px-0">
+                <input
+                  type="number"
+                  className="input-Container w-100"
+                  placeholder="Fare per day"
+                />
+              </div>
+              <div className="col-12 my-2 px-0">
+                <button className="regular-btn">
+                  Add to Garage
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
-    );
-  };
-  
+    </div>
+  );
+};

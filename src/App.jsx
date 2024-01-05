89573b638b74db403aebf753f08e4e451473ee9a
John Anthony <johnrico364@gmail.com>
2024-01-05 23:12:04 +0800

    Add: Setup react query

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { useState, createContext } from "react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+// Routes
 import { Login } from "./pages/Login";
 import { Signup } from "./pages/Signup";
 import { UserNavbar } from "./pages/user/UserNavbar";
 import { RentCar } from "./pages/user/RentCar";
 import { ProfilePage } from "./pages/user/Profile";
 import { Garage } from "./pages/user/Garage";
-import { useState, createContext } from "react";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { AdminNavbar } from "./pages/admin/AdminNavbar";
 import { RentList } from "./pages/admin/RentList";
 import { AddCar } from "./pages/admin/AddCar";
@@ -22,25 +24,27 @@ function App() {
 
   return (
     <div className="App">
-      <AppContext.Provider value={{ userData, setUserData }}>
-        <Router>
-          <Routes>
-            <Route path="user" element={<UserNavbar />}>
-              <Route path="rent" element={<RentCar />} />
-              <Route path="profile" element={<ProfilePage />} />
-              <Route path="garage" element={<Garage />} />
-            </Route>
-            <Route path="admin" element={<AdminNavbar/>}>
-              <Route path="list" element={<RentList/>}/>
-              <Route path="add-car" element={<AddCar/>}/>
-              <Route path="dashboard" element={<Dashboard/>}/>
-            </Route>
+      <QueryClientProvider client={client}>
+        <AppContext.Provider value={{ userData, setUserData }}>
+          <Router>
+            <Routes>
+              <Route path="user" element={<UserNavbar />}>
+                <Route path="rent" element={<RentCar />} />
+                <Route path="profile" element={<ProfilePage />} />
+                <Route path="garage" element={<Garage />} />
+              </Route>
+              <Route path="admin" element={<AdminNavbar />}>
+                <Route path="list" element={<RentList />} />
+                <Route path="add-car" element={<AddCar />} />
+                <Route path="dashboard" element={<Dashboard />} />
+              </Route>
 
-            <Route path="" element={<Login />} />
-            <Route path="signup" element={<Signup />} />
-          </Routes>
-        </Router>
-      </AppContext.Provider>
+              <Route path="" element={<Login />} />
+              <Route path="signup" element={<Signup />} />
+            </Routes>
+          </Router>
+        </AppContext.Provider>
+      </QueryClientProvider>
     </div>
   );
 }

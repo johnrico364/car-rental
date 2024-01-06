a41dde86c4de9bc216b1d9b3764e8b315eb0b16a
John Anthony <johnrico364@gmail.com>
2024-01-06 13:22:13 +0800

    Add: available car table UI

--- src/pages/admin/RentList.jsx
+++ src/pages/admin/RentList.jsx
@@ -8,13 +8,13 @@ export const RentList = () => {
         <div className="horizontal-line"></div>
         <div className="row rented-values-container justify-content-center overflow-auto">
           <div className="col-11">
-            <div className="row my-2">
-              <div className="col-4 rented-values-title">Rented By</div>
-              <div className="col-2 rented-values-title">Car Name</div>
-              <div className="col-1 rented-values-title">Category</div>
-              <div className="col-1 rented-values-title">Start</div>
-              <div className="col-2 rented-values-title">Return</div>
-              <div className="col-2 rented-values-title">Fare</div>
+            <div className="row rented-values-title my-2">
+              <div className="col-4">Rented By</div>
+              <div className="col-2">Car Name</div>
+              <div className="col-1">Category</div>
+              <div className="col-1">Start</div>
+              <div className="col-2">Return</div>
+              <div className="col-2">Fare</div>
             </div>
             <div className="row mb-2 rented-item">
               <div className="col-4 rented-values">
--- src/pages/user/RentCar.jsx
+++ src/pages/user/RentCar.jsx
@@ -33,10 +33,26 @@ export const RentCar = () => {
               </div>
             </div>
             <div className="rent-car-title mt-3">Available Cars</div>
-            <div className="row">
-              <div className="col-12 px-4">
-                <div className="available-car-container overflow-auto ">
-
+            <div className="row available-car-table justify-content-center overflow-auto px-3">
+              <div className="col-12">
+                <div className="row available-car-title">
+                  <div className="col-3 available-car-values ps-5">Name</div>
+                  <div className="col-2 available-car-values">Color</div>
+                  <div className="col-2 available-car-values">Category</div>
+                  <div className="col-2 available-car-values">Fare</div>
+                  <div className="col-3 text-end"></div>
+                </div>
+                <div className="row available-car-items">
+                  <div className="col-3 available-car-values">
+                    <i className="bi bi-car-front-fill me-4"></i>
+                    Toyota Supra
+                  </div>
+                  <div className="col-2 available-car-values">Red</div>
+                  <div className="col-2 available-car-values">Coupe</div>
+                  <div className="col-2 available-car-values">P 3000</div>
+                  <div className="col-3 text-end">
+                    <button className="rent-button">Rent</button>
+                  </div>
                 </div>
               </div>
             </div>
